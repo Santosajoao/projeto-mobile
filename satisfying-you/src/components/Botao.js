04f86e5b6722a2d2983e467bf40ee3b7e89ebd37
Joao Araujo <joaosantos162003@gmail.com>
2024-11-12 15:37:22 -0300

    adicionando propriedades no componente do botão

--- satisfying-you/src/components/Botao.js
+++ satisfying-you/src/components/Botao.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';  // Importando o hook useRouter
 
-const Botao = ({ onPress, title, color, size, textColor, fontFamily, link }) => {
+const Botao = ({ onPress, title, color, size, textColor, fontFamily, link, marginBottom, marginTop }) => {
   const router = useRouter(); 
 
   const handlePress = () => {
@@ -15,7 +15,7 @@ const Botao = ({ onPress, title, color, size, textColor, fontFamily, link }) => 
   };
 
   return (
-    <TouchableOpacity style={[styles.button, { backgroundColor: color }]} onPress={handlePress}>
+    <TouchableOpacity style={[styles.button, { backgroundColor: color, marginBottom: marginBottom || 10, marginTop: marginTop }]} onPress={handlePress}>
       <Text style={[{ color: textColor, fontSize: size, fontFamily: fontFamily }]}>{title}</Text>
     </TouchableOpacity>
   );
@@ -26,7 +26,6 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     alignItems: 'center',
-    marginBottom: 10,
   },
 });
 
