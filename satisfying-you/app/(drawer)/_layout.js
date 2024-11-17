93774e4ef6813b2f7eb2eedb0a39c8325f21e032
Joao Araujo <joaosantos162003@gmail.com>
2024-11-17 16:05:15 -0300

    Configurando drawer navigation

--- satisfying-you/app/(drawer)/_layout.js
+++ satisfying-you/app/(drawer)/_layout.js
@@ -1,12 +1,15 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Drawer } from 'expo-router/drawer';
-import { useFonts } from 'expo-font';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import { useFonts } from "expo-font";
 import { useEffect } from "react";
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from "expo-splash-screen";
+import { MaterialIcons } from "@expo/vector-icons";
+import CustomDrawerContent from "../../src/components/customDrawer";
 
 export default function Layout() {
   const [loaded, error] = useFonts({
-    'AveriaLibre': require('../../assets/fonts/AveriaLibre-Regular.ttf'),
+    AveriaLibre: require("../../assets/fonts/AveriaLibre-Regular.ttf"),
+    "AveriaLibre-Light": require("../../assets/fonts/AveriaLibre-Light.ttf"),
   });
 
   useEffect(() => {
@@ -18,25 +21,57 @@ export default function Layout() {
   if (!loaded && !error) {
     return null;
   }
-  
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer
+        drawerContent={CustomDrawerContent}
         screenOptions={{
-          headerStyle: {
-            backgroundColor: '#2B1D62',
-            borderBottomColor: '#2B1D62',
+          drawerStyle: {
+            backgroundColor: "#372775", // Cor de fundo do Drawer
+            width: 260, // Largura do Drawer
           },
-  
-          headerTintColor: '#fff',
-          
+          drawerLabelStyle: {
+            fontFamily: "AveriaLibre",
+            fontSize: 16,
+            color: "#fff",
+          },
+
+          drawerInactiveTintColor: "#fff", // Cor dos itens não selecionados
+          drawerActiveTintColor: "#000",
+
+          drawerItemStyle: {
+            borderRadius: 0, // Remove o borderRadius
+            backgroundColor: "transparent", // Remove a cor de fundo
+          },
+
+          headerStyle: {
+            backgroundColor: "#2B1D62",
+            borderBottomColor: "#2B1D62",
+          },
+
+          headerTintColor: "#fff",
         }}
       >
         <Drawer.Screen
-          name="index" // This is the name of the page and must match the url from root
+          name="index"
           options={{
-            drawerLabel: 'Home',
-            title: '',
+            drawerLabel: "Pesquisa",
+            title: "",
+            drawerIcon: ({ size }) => (
+              <MaterialIcons name="description" size={size} color={"#fff"} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="autenticacao/login"
+          options={{
+            drawerLabel: "Sair",
+            title: "Sair",
+            drawerIcon: ({ size }) => (
+              <MaterialIcons name="logout" size={size} color={"#fff"} />
+            ),
+            headerShown: false,
           }}
         />
       </Drawer>
