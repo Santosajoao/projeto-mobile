4435e87dfc71c576a5ce9c41e972c855cfe2b131
Joao Araujo <joaosantos162003@gmail.com>
2024-11-17 16:05:39 -0300

    Estruturando home

--- satisfying-you/app/(drawer)/index.js
+++ satisfying-you/app/(drawer)/index.js
@@ -1,29 +1,48 @@
 import React from "react";
 import { View, TextInput, StyleSheet, ScrollView, Text } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import Botao from "../../src/components/Botao";
 
 const Home = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      {/* Search Input no topo */}
       <View style={styles.searchContainer}>
-        <Icon name="search" size={25} color="#8B8B8B" style={styles.searchIcon} />
+        <Icon
+          name="search"
+          size={25}
+          color="#8B8B8B"
+          style={styles.searchIcon}
+        />
         <TextInput
           style={styles.searchInput}
           placeholder="Insira o termo de busca..."
         />
       </View>
 
+      {/* Cards */}
       <View style={styles.cardContainer}>
         <View style={styles.card}>
-          <Text>Card 1</Text>
+          <Icon name="devices" size={100} color="#704141" />
+          <Text style={styles.cardText}>SECOMP 2023</Text>
+          <Text style={styles.cardCaption}>10/10/2023</Text>
         </View>
         <View style={styles.card}>
-          <Text>Card 2</Text>
+          <Text style={styles.cardText}>Card 2</Text>
         </View>
         <View style={styles.card}>
-          <Text>Card 3</Text>
+          <Text style={styles.cardText}>Card 3</Text>
         </View>
       </View>
+      <Botao
+        color={"#37BD6D"}
+        fontFamily={"AveriaLibre"}
+        title={"Nova Pesquisa".toUpperCase()}
+        textColor={"#fff"}
+        size={20}
+        marginTop={30}
+        // onPress={handleCreateAccount}
+      />
     </ScrollView>
   );
 };
@@ -52,22 +71,32 @@ const styles = StyleSheet.create({
     fontFamily: "AveriaLibre",
   },
   cardContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexGrow: 1,
+    justifyContent: "center",
     alignItems: "center",
-    marginBottom: 16,
+    flexDirection: "row",
+    gap: 20,
   },
   card: {
-    backgroundColor: "#f9f9f9",
-    padding: 16,
-    borderRadius: 5,
-    marginBottom: 16,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
+    width: 271,
+    height: 238,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
     elevation: 3,
-    flex: 1,
-    marginHorizontal: 5,
+    marginBottom: 20,
+  },
+  cardText: {
+    marginTop: 8,
+    fontSize: 16,
+    color: "#3F92C5",
+    fontFamily: "AveriaLibre-Light",
+  },
+  cardCaption: {
+    color: "#8B8B8B",
+    fontFamily: "AveriaLibre",
+    fontSize: 12,
   },
 });
 
