b65d2dab8d50e56f133f393202ea354cc0ba5bb8
Joao Araujo <joaosantos162003@gmail.com>
2025-01-25 21:55:06 -0300

    Wrap RootLayout component with Redux Provider to enable state management

--- satisfying-you/app/_layout.js
+++ satisfying-you/app/_layout.js
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,61 +23,63 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#2B1D62",
-          borderBottomColor: "#2B1D62",
-        },
+    <Provider store={store}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#2B1D62",
+            borderBottomColor: "#2B1D62",
+          },
 
-        headerTintColor: "#573FBA",
+          headerTintColor: "#573FBA",
 
-        headerTitleStyle: {
-          fontFamily: "AveriaLibre",
-          color: "#fff",
-          fontSize: 30,
-        },
-      }}
-    >
-      <Stack.Screen
-        name="autenticacao/login"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="autenticacao/criarConta"
-        options={{ title: "Nova Conta" }}
-      />
-      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="autenticacao/esqueciSenha"
-        options={{ title: "Recuperação de senha" }}
-      />
-      <Stack.Screen
-        name="pesquisa/novaPesquisa"
-        options={{ title: "Nova pesquisa" }}
-      />
-      <Stack.Screen
-        name="pesquisa/modificarPesquisa"
-        options={{ title: "Modificar pesquisa" }}
-      />
-      <Stack.Screen
-        name="pesquisa/acoesPesquisa"
-        options={{ title: "Carnaval" }}
-      />
+          headerTitleStyle: {
+            fontFamily: "AveriaLibre",
+            color: "#fff",
+            fontSize: 30,
+          },
+        }}
+      >
+        <Stack.Screen
+          name="autenticacao/login"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="autenticacao/criarConta"
+          options={{ title: "Nova Conta" }}
+        />
+        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="autenticacao/esqueciSenha"
+          options={{ title: "Recuperação de senha" }}
+        />
+        <Stack.Screen
+          name="pesquisa/novaPesquisa"
+          options={{ title: "Nova pesquisa" }}
+        />
+        <Stack.Screen
+          name="pesquisa/modificarPesquisa"
+          options={{ title: "Modificar pesquisa" }}
+        />
+        <Stack.Screen
+          name="pesquisa/acoesPesquisa"
+          // options={{ title: "Carnaval" }}
+        />
 
-      <Stack.Screen
-        name="pesquisa/coletarDados"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="pesquisa/agradecimentoParticipacao"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="pesquisa/relatorio"
-        options={{ title: "Relatório" }}
-      />
-    </Stack>
+        <Stack.Screen
+          name="pesquisa/coletarDados"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="pesquisa/agradecimentoParticipacao"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="pesquisa/relatorio"
+          options={{ title: "Relatório" }}
+        />
+      </Stack>
+    </Provider>
   );
 }
